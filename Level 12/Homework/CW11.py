eb9804184774b9cf728d91f891d2f9223dd9107f
Frazzy01 <levaniko_beridze@mail.ru>
2025-11-07 19:21:10 +0400

    twelfth commit

---
+++ Level 12/Homework/CW11.py
@@ -0,0 +1,14 @@
+# 11)კომენტარის სახით ჩამოწერეთ ის სამი ტექნიკა რასაც კომპიუტერი იყენებს პრობლემების გადასაჭრელად კოდში,თითეულს მიუწერე თავიანთ ახსნა განმარტება გვერდით
+
+# 1) Sequence – es aris moqmedebis shesruleba zedized, zustad im rigit rogorc kodshia dawerili
+#    anu programa kods kitxulobs zemodan qvemot da asrulebs titoeul brdzanebas tanmimdevrobit
+#    Sequence = tanmimdevroba
+
+# 2) Selection – es aris gadawyvetilebis migebis procesi, sadac programa ragac pirobis mixedvit irchevs gzas
+#    magalitad if/else pirobiti operatorebi – tu piroba weshmaritia erti kodi sruldeba, tu ara – meore
+#    Selection = pirobiti archevani
+
+# 3) Iteration – igive cikli, es aris moqmedebis ganmeoreba manam sanam piroba shesrulebulia
+#    magalitad, for cikli moqmedebis ganmeorebit asrulebs garkveul raodenobis ganmavlobashi 
+#    romelic winaswar aris gansazvgruli
+#    Iteration = ganmeoreba (cikli)
