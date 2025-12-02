c79fa7bd06d4eb62acab96f826f67d7cae1f93fe
Frazzy01 <levaniko_beridze@mail.ru>
2025-12-02 21:15:32 +0400

    eighteenth commit

--- Level 18/Homework/HW6.py
+++ Level 18/Homework/HW6.py
@@ -1,3 +1,6 @@
+# 6)კომენტარის სახით ახსენით თუ რა არის სია და რაშივიყენებთ მას,ასევე ახსენი თუ რით განსხვავდება იგი ცვლადისგან
+
+
 # siebi aris ert erti monacemta tipi siashi shegvidzlia shevnitaxot ,ramdeni elementic minda imdeni elementi shegvidzlia shevitanot
 # da ra monacemta tipis elemntic minda mag: integer, string, float, boolean
 
