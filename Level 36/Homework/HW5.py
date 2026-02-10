8252fc19e2accb9f5bc7d01f4fe92b53fd1c119c
Frazzy01 <levaniko_beridze@mail.ru>
2026-02-10 21:15:53 +0400

    Thirty-sixth commit

--- Level 36/Homework/HW5.py
+++ Level 36/Homework/HW5.py
@@ -1,8 +1,10 @@
 # 5) შექმენი ფუქნცია რომელიც იღებს რიცხვების სიას და აბრუნებს მათ საშუალოს
 
 def sia(numbers):
-    average = sum(numbers) / len(numbers)
-    return average
+    sul = 0
+    for i in numbers:
+        sul += i
+    return sul / len(numbers)
 
 numbers = [322, 101, 52, 44, 69]
 print(sia(numbers))
