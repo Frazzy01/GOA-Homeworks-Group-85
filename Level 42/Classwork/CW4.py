0e42a4e3d3ec99236989e752a5db88d91da61f8b
Frazzy01 <levaniko_beridze@mail.ru>
2026-02-25 19:46:38 +0400

    forty-second commit

---
+++ Level 42/Classwork/CW4.py
@@ -0,0 +1,6 @@
+# def twice_as_old(d,s):
+#     age = d - s*2
+#     if age <0:
+#         return -age
+#     else:
+#         return age
---
+++ Level 42/Classwork/CW5.py
@@ -0,0 +1,7 @@
+# def number(bus_stops):
+#     get_on = 0
+#     get_off = 0
+#     for i in bus_stops:
+#         get_on += i[0]
+#         get_off += i[-1]
+#     return get_on - get_off
