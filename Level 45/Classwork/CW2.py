3973c00591096f6867b9c2c0e218887f8cc63f6b
Frazzy01 <levaniko_beridze@mail.ru>
2026-03-05 23:04:43 +0400

    fourty-fifth commit

---
+++ Level 45/Classwork/CW2.py
@@ -0,0 +1,5 @@
+def count_sheep(n):
+    word = ''
+    for i in range(1, n+1):
+        word += str(i) + " sheep..."
+    return word
---
+++ Level 45/Classwork/CW5.py
@@ -0,0 +1,6 @@
+def friend(x):
+    new = []
+    for i in x:
+        if len(i)==4:
+            new.append(i)
+    return new
