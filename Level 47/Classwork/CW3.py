0e00408cc3a20243284478be8e1c4518c6fa40a6
Frazzy01 <levaniko_beridze@mail.ru>
2026-03-12 22:59:27 +0400

    fourty-seventh commit

---
+++ Level 47/Classwork/CW3.py
@@ -0,0 +1,9 @@
+# def descending_order(num):
+#     l1 = []
+#     for i in str(num):
+#         l1.append(int(i))
+#     b = ""
+#     while l1:
+#         b += str(max(l1))
+#         l1.remove(max(l1))
+#     return int(b)
