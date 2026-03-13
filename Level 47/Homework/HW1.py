ba89b0f3f187c0955fda4b9f3a78d41d11f16518
Frazzy01 <levaniko_beridze@mail.ru>
2026-03-13 14:47:54 +0400

    fourty-seventh commit

---
+++ Level 47/Homework/HW1.py
@@ -0,0 +1,13 @@
+# # write the function is_anagram
+# def is_anagram(test, original):
+#     test = test.lower()
+#     original = original.lower()
+
+#     if len(test) != len(original):
+#         return False
+
+#     for i in test:
+#         if test.count(i) != original.count(i):
+#             return False
+
+#     return True
