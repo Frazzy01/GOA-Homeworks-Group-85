5d2b6b5923e294c3ea6d6a2ca6b4c6912fe62795
Frazzy01 <levaniko_beridze@mail.ru>
2026-03-20 18:36:39 +0400

    fourty-ninth commit

---
+++ Level 49/Homework/HW2.py
@@ -0,0 +1,8 @@
+# def square_digits(num):
+#     result = 0
+    
+#     for i in str(num):
+#         squared = int(i) * int(i)
+#         result = result * (10 ** len(str(squared))) + squared
+        
+#     return result
---
+++ Level 49/Homework/HW5.py
@@ -0,0 +1,11 @@
+# def xo(s):
+#     x_count = 0
+#     o_count = 0
+    
+#     for i in s:
+#         if i == 'x' or i == 'X':
+#             x_count += 1
+#         elif i == 'o' or i == 'O':
+#             o_count += 1
+    
+#     return x_count == o_count
