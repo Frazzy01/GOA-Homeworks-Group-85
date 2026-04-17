ed22f2c053ea9b77823254e696ae56da0973c74f
Frazzy01 <levaniko_beridze@mail.ru>
2026-04-17 18:56:30 +0400

    fifty-seventh

---
+++ Level 57/Homework/HW5.py
@@ -0,0 +1,13 @@
+# def capitalize(s):
+#     even = ''
+#     odd = ''
+    
+#     for i in range(len(s)):
+#         if i % 2 == 0:
+#             even += s[i].upper()
+#             odd += s[i]
+#         else:
+#             even += s[i]
+#             odd += s[i].upper()
+    
+#     return [even, odd]
---
+++ Level 57/Homework/HW8.py
@@ -0,0 +1,9 @@
+# def solution(digits):
+#     max_num = 0
+    
+#     for i in range(len(digits) - 4):
+#         num = int(digits[i:i+5])
+#         if num > max_num:
+#             max_num = num
+    
+#     return max_num
