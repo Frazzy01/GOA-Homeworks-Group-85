6cac85a9d67376d8a8b72ddd03c4895df1d2d030
Frazzy01 <levaniko_beridze@mail.ru>
2026-05-03 13:45:14 +0400

    sixty-second commit

---
+++ Level 62/Classwork/CW1.py
@@ -0,0 +1,8 @@
+# def number(lines):
+#     result = []
+
+#     for i in range(len(lines)):
+#         line = str(i + 1) + ": " + lines[i]
+#         result.append(line)
+
+#     return result
---
+++ Level 62/Classwork/CW2.py
@@ -0,0 +1,15 @@
+# def reverse_number(n):
+#     if n < 0:
+#         sign = -1
+#     else:
+#         sign = 1
+
+#     n = abs(n)
+    
+#     reversed_num = 0
+#     while n != 0:
+#         digit = n % 10
+#         reversed_num = reversed_num * 10 + digit
+#         n //= 10
+    
+#     return sign * reversed_num
