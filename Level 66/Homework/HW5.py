a27e9786633c52b7d74b7aa3ff810656a140a656
Frazzy01 <levaniko_beridze@mail.ru>
2026-05-15 21:58:19 +0400

    sixty-sixth commit

---
+++ Level 66/Homework/HW5.py
@@ -0,0 +1,7 @@
+# def incrementer(nums):
+#     result = []
+
+#     for i in range(len(nums)):
+#         result.append((nums[i] + (i + 1)) % 10)
+
+#     return result
---
+++ Level 66/Homework/HW7.py
@@ -0,0 +1,6 @@
+# def largest(n, xs):
+#     if n == 0:
+#         return []
+#     else:
+#         return sorted(xs)[-n:]
+        
