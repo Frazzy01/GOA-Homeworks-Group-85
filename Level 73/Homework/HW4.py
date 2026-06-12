871e56c26ee1a93296c961ebe2a45d4931ea0ca4
Frazzy01 <levaniko_beridze@mail.ru>
2026-06-12 23:13:13 +0400

    seventy-third commit

---
+++ Level 73/Homework/HW4.py
@@ -0,0 +1,10 @@
+# def find_longest(arr):
+#     best = arr[0]
+#     maxlen = len(str(arr[0]))
+    
+#     for i in arr:
+#         if len(str(i)) > maxlen:
+#             best = i
+#             maxlen = len(str(i))
+    
+#     return best
