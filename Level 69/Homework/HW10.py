7c29de15a80d2d6d5bb93bea72aedd5fb27aa2d6
Frazzy01 <levaniko_beridze@mail.ru>
2026-06-25 20:40:14 +0400

    sixty-ninth commit

---
+++ Level 69/Homework/HW10.py
@@ -0,0 +1,15 @@
+def is_nice(arr):
+    if len(arr) == 0:
+        return False
+        
+    for n in arr:
+        has_neighbor = False
+        
+        for other in arr:
+            if other == n - 1 or other == n + 1:
+                has_neighbor = True
+                
+        if not has_neighbor:
+            return False
+            
+    return True
