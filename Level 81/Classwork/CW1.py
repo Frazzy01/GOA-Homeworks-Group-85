76830d9019eedb0c3fb077cdeb9433b26a4ba4f0
Frazzy01 <levaniko_beridze@mail.ru>
2026-07-16 20:59:11 +0400

    eighty-first commit

---
+++ Level 81/Classwork/CW1.py
@@ -0,0 +1,11 @@
+# def calc_type(a, b, res) -> str:
+#     if a * b == res:
+#         return "multiplication"
+#     elif a + b == res:
+#         return "addition"
+#     elif a - b == res:
+#         return "subtraction"
+#     elif b != 0 and a / b == res:
+#         return "division"
+#     else:
+#         return "idk"
