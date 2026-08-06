c1543bbefbef4605715eab9066a771005ab9a0ce
Frazzy01 <levaniko_beridze@mail.ru>
2026-08-06 12:26:16 +0400

    eighty-sixth commit

--- Level 86/Homework/HW7.js
+++ Level 86/Homework/HW7.js
@@ -1,6 +1,6 @@
 /* console.log("<3") string <3
 console.log(69) number <3
 console.log(true) boolean <3
-console.log(2 ** 199 - 1) bigint <3
+console.log(2 * 199 - 1) bigint <3
 console.log(null) null <3
 console.log(undefined) undefined <3 */
