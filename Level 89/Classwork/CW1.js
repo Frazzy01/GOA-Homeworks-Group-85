b500846c7ef4cbe08487390ade8e6b62cd374933
Frazzy01 <levaniko_beridze@mail.ru>
2026-08-07 13:52:05 +0400

    eighty-ninth commit

---
+++ Level 89/Classwork/CW1.js
@@ -0,0 +1,8 @@
+console.log("Levan ".repeat(15))
+
+
+console.log("i am going to dubai".replace("dubai", "Tbilisi"))
+
+
+
+console.log("my friend told me to tell my other friend to come".replaceAll("friend", "bro"))
---
+++ Level 89/Classwork/CW2.js
@@ -0,0 +1,15 @@
+let name
+
+console.log(name)
+
+name = "levanBeridze"
+
+console.log(name)
+
+// ===============================
+
+const idk = 52
+
+console.log(idk)
+
+// const itargmneba rogorc mudmivi anu magalitad let-shi sheiddzleba shecvla xolo const mudmivvv anu rac eqneba ise eqneba sul
