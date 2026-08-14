3293e1b9cdbeb12f5e401d30f71e6d63c5a2398f
Frazzy01 <levaniko_beridze@mail.ru>
2026-08-14 19:09:20 +0400

    ninth-first commit

---
+++ Level 91/Classwork/CW1.js
@@ -0,0 +1,17 @@
+let number = 999
+
+if(number > 0){
+    console.log("positive number")
+}else if(age < 0) {
+    console.log("negative number")
+}else{
+    console.log("zero")
+}
+
+let name = "levan"
+
+if(name === "levan"){
+    console.log("we have same name")
+}else{
+    console.log("we do not have same name")
+}
---
+++ Level 91/Classwork/CW2.js
@@ -0,0 +1,17 @@
+let number = 999
+
+if(number > 0 && number % 2 == 0){
+    console.log("positive and even")
+}else{
+    console.log("other number")
+}
+
+let name = "levani"
+
+if(name.startsWith("g") || name == "levani"){
+    console.log("good name")
+}else if(name.startsWithv("a") && name == "akaki"){
+    console.log("excellent name")
+}else{
+    console.log("other name")
+}
