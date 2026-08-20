acb02697b635d34138b3abc9bb15c9eb980fef44
Frazzy01 <levaniko_beridze@mail.ru>
2026-08-20 12:16:51 +0400

    ninth-second commit

--- Level 92/Homework/HW9.js
+++ Level 92/Homework/HW9.js
@@ -1,16 +1,16 @@
 let month = 8
 
 switch(month){
-    case 1 || 2 || 12:
+    case 1, 2, 12 :
         console.log("Winter")
         break
-    case 3 || 4 || 5:
+    case 3, 4, 5 :
         console.log("Spring")
         break
-    case 6 || 7 || 8:
+    case 6, 7, 8 :
         console.log("Summer")
         break
-    case 9 || 10 || 11:
+    case 9, 10, 11:
         console.log("Autumn")
         break
     default:
