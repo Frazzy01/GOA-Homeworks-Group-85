c45e616aa32a2477e6d5c869b602d8e31d9698c9
Frazzy01 <levaniko_beridze@mail.ru>
2026-08-25 12:50:49 +0400

    ninth-third commit

---
+++ Level 93/Classwork/CW1.js
@@ -0,0 +1,5 @@
+function myName(){
+    console.log("Levan Beridze")
+}
+
+myName()
---
+++ Level 93/Classwork/CW2.js
@@ -0,0 +1,8 @@
+function punction(name = "Levan", surname = "beridze", age = 15){
+    console.log("Hello my name is", name , "and my surname is", surname, "my age is", age, )
+}
+
+punction("guram")
+punction("guram", "razmadze")
+punction("guram", "razmadze", 16)
+punction()
