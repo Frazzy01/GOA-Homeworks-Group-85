05e0de909082672ec13b8efeff0c14ac4682648e
Frazzy01 <levaniko_beridze@mail.ru>
2026-08-27 12:19:16 +0400

    ninth-third commit

--- Level 93/Homework/HW8.js
+++ Level 93/Homework/HW8.js
@@ -2,7 +2,7 @@ function convertTemperature(value = 30, type = "C"){
     if(type === "C"){
         return value*9/5+32
     }else{
-        return (value-32)*5/9d
+        return (value-32)*5/9
     }
 }
 
