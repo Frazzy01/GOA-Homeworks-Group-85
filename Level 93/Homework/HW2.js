84c7bdb8830a93fe9e72a400a9d0c4599cd7750e
Frazzy01 <levaniko_beridze@mail.ru>
2026-08-27 12:15:37 +0400

    ninth-third commit

--- Level 93/Homework/HW2.js
+++ Level 93/Homework/HW2.js
@@ -1,21 +1,15 @@
-function getResult(name, score = 0){
-    switch(true){
-        case(score >= 90):
-            console.log("name: Excellent")
-            break
-        case(score >= 70):
-            console.log("name: Good")
-            break
-        case(score >= 50):
-            console.log("name: Passed")
-            break
-        case(score <= 49):
-            console.log("name: Failed")
-            break
-        default:
-            console.log("0")
-            break
-    }   
+function getResult(name, score = 0) {
+    if(score >= 90){
+        console.log("name: Excellent")
+    }else if(score >= 70){
+        console.log("name: Good")
+    }else if(score >= 50){
+        console.log("name: Passed")
+    }else if(score >= 1){
+        console.log("name: Failed")
+    }else{
+        console.log(score)
+    }
 }
 
 getResult("Levan", 1000)
@@ -23,6 +17,6 @@ getResult("Levan", 89)
 getResult("Levan", 70)
 getResult("Levan", 69)
 getResult("Levan", 50)
-getResult("Levan", 49)
-getResult("Levan",)
+getResult("Levan", 34)
+getResult("Levan", )
 getResult("Levan",)
