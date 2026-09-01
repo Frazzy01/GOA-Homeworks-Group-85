8805c2fc8cdbbccd1978e64ec275d75cbdb82034
Frazzy01 <levaniko_beridze@mail.ru>
2026-09-01 10:02:07 +0400

    ninth-fourth commit

--- Level 94/Homework/HW8.js
+++ Level 94/Homework/HW8.js
@@ -1,5 +1,12 @@
 let validatePassword = password => {
-    if(password.lenght() >= 8 && password.test("@") && password.startsWith()){
-
+    if(password.length >= 8 && password.includes("@") && password.startsWith(password[0].toUpperCase())){
+        console.log("Strong password")
+    }else{
+        console.log("Weak password")
     }
 }
+
+validatePassword("Simple@@@@@@@@")
+validatePassword("simple@@@@@@@@")
+validatePassword("simpleeeeeeeeee")
+validatePassword("lol")
