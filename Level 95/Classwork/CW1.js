3082b382beb3dea8d0a1887032f04b44a7b7bb6f
Frazzy01 <levaniko_beridze@mail.ru>
2026-09-03 13:50:22 +0400

    ninty-fifth commit

---
+++ Level 95/Classwork/CW1.js
@@ -0,0 +1,7 @@
+const qwerty = ["qwertyqwerty", 69, true, "", 99*9999999999999999999999999999999999999999999,]
+
+console.log(qwerty[0])
+console.log(qwerty[1])
+console.log(qwerty[2])
+console.log(qwerty[3])
+console.log(qwerty[4])
---
+++ Level 95/Classwork/CW2.js
@@ -0,0 +1,12 @@
+let scores = [45, 78, 32, 90, 56, 67, 23, 84, 39, 71, 60, 95]
+
+scores[0] += 15
+scores[2] = 50
+scores[6] += 20
+scores[8] *= 2
+scores[10] -= 10
+scores[11] = 100
+scores[5] = 75
+scores[1] += 6
+
+console.log(scores)
