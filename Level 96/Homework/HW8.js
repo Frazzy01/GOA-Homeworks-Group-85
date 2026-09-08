3a9d357f273c81b24af4e0660bb52e6b327cb527
Frazzy01 <levaniko_beridze@mail.ru>
2026-09-08 10:28:21 +0400

    ninty-sixth commit

--- Level 96/Homework/HW8.js
+++ Level 96/Homework/HW8.js
@@ -1,7 +1,9 @@
 let numbers = [10, 20, 30, 40, 50, 60, 70, 80]
 
-if(numbers[0, 1, 2, 3, 4, 5, 6, 7] % 2 == 0){
-    numbers * 5
+if(numbers){
+    numbers[numbers % 2 == 0] * 5
+    console.log(numbers)
 }else{
-    numbers * 2
+    numbers[numbers % 2 != 0] * 2
+    console.log(numbers)
 }
