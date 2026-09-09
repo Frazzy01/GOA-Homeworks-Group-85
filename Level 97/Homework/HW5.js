9be7f512ad93eb6e48ddb00ff3a9f18693ff6c93
Frazzy01 <levaniko_beridze@mail.ru>
2026-09-09 22:07:02 +0400

    ninty-sixth commit

---
+++ Level 97/Homework/HW5.js
@@ -0,0 +1,8 @@
+let products = ["Laptop", "Phone", "Tablet", "Watch", "Headphones", "Camera"]
+let productsjumber = products.slice(0, 3)
+let productsjumberiko = products.slice(3, 6)
+let productsjumberikosito = products.slice(1, 4)
+
+console.log(productsjumber)
+console.log(productsjumberiko)
+console.log(productsjumberikosito)
