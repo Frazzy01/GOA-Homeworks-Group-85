515cfae98676597e0b05cfe3de72989abafb7362
Frazzy01 <levaniko_beridze@mail.ru>
2026-09-10 12:27:12 +0400

    ninty-seventh commit

--- Level 97/Homework/HW17.js
+++ Level 97/Homework/HW17.js
@@ -1,7 +1,6 @@
 let morning = ["Math", "English"]
 let afternoon = ["History", "Physics"]
 let evening = ["Programming", "Design"]
-
-morning = morning.concat(afternoon, evening)
+let sagnebijumber = morning.concat(afternoon, evening)
 
 console.log(morning)
