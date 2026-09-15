66529b76f7cbe549fc97994a0494e3d7a495ea76
Frazzy01 <levaniko_beridze@mail.ru>
2026-09-15 23:01:25 +0400

    ninty-ninth commit

---
+++ Level 99/Classwork/CW1.js
@@ -0,0 +1,23 @@
+// let namerjumber = ['goga', 'levan', 'tato', 'andria', 'jimsheri', 'gurami']
+// for(let i = 0; i < namerjumber.length; i++){
+//     if(namerjumber[i].length > 5 && namerjumber[i].startsWith('g')){
+//         console.log(namerjumber[i])
+//     }
+// }
+
+
+
+// let numberjumber = [999, 100, 222, 42, 69]
+
+// for (let i = 0; i < numberjumber.length; i++) {
+//   if(numberjumber[i] >= 100 && numberjumber[i] % 2 == 0) {
+//     console.log(numberjumber[i])
+//   }
+// }
+
+
+// let stringerjumber = ["levan", "andria", "tato", "jimsheri", "goga"]
+
+// for(let i = 0; i < stringerjumber.length; i++){
+//     console.log(i, stringerjumber[i])
+// }
