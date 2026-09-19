19e7a0cd9cac63cbd7902b0aaab0cec786352435
Frazzy01 <levaniko_beridze@mail.ru>
2026-09-19 16:11:07 +0400

    hundred commit

---
+++ Level 100/Classwork/CW1.js
@@ -0,0 +1,35 @@
+// let namesjumber = ["Levan", "Tat", "Andria", "Gog", "jimsheri"]
+
+
+// let i = 0
+// while(i < namesjumber.length){
+//     if(namesjumber[i].length <= 4){
+//         console.log(namesjumber[i])
+//     }
+
+//     i++
+// }
+
+// let namesjumber = ["Levan", "Tat", "Andria", "Gog", "jimsheri"];
+// let i = 0;
+
+// do {
+    
+//     if (namesjumber[i].length <= 4) {
+//         console.log(namesjumber[i]);
+//     }
+    
+//     i++;
+// } while (i < namesjumber.length);
+
+
+// let numberjumber = [69, 100, 141, 52, 42]
+
+// for(let i = 0; i < numberjumber.length; i++){
+//     if(numberjumber[i] > 50){
+//         console.log("num which is greater than 50 is found", numberjumber[i])
+//         break
+//     }
+
+//     console.log(numberjumber[i])
+// }
