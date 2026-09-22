51878ac538afdde0981cdfdeac571be161c59209
Frazzy01 <levaniko_beridze@mail.ru>
2026-09-22 22:06:15 +0400

    hundred commit

--- Level 100/Homework/HW15.js
+++ Level 100/Homework/HW15.js
@@ -2,18 +2,21 @@ let numbers = [15, 8, 23, 42, 11, 67, 30, 19, 54, 72, 5]
 let count = 0
 let i = 0
 while (i < numbers.length) {
-    i++
+
+    if (numbers[i] % 2 !== 0) {
+        i++
+        continue
+    }
 
     if (numbers[i] > 50) {
         console.log(numbers[i])
         break
     }
 
-    if (numbers[i] % 2 !== 0) { continue }
-
     console.log(numbers[i])
     count += numbers[i]
 
+    i++
 }
 
 console.log(count)
